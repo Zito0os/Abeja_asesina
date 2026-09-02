78fd9754e4d5b1232b6d14fa84812f57034d76b6
Yepo <tonbarrz5@gamil.com>
2026-09-02 10:23:22 -0600

    Pruebas de animacion (no esta terminado)

---
+++ src/components/animation.jsx
@@ -0,0 +1,10 @@
+import Lottie from "lottie-react";
+import paperUnfoldAnimation from "../assets/";
+
+export const PaperUnfold = () => (
+  <Lottie 
+    animationData={paperUnfoldAnimation} 
+    loop={false}
+    autoplay={true}
+  />
+);
