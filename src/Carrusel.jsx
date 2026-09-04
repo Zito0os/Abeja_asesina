ae7c1432bd6ee2c3c9ec62d1609eac2a60be2101
Betsary <paolabetsabehdzr@gmail.com>
2026-09-03 19:08:26 -0600

    imagenes

--- src/Carrusel.jsx
+++ src/Carrusel.jsx
@@ -6,26 +6,31 @@ const bees = [
     id: 1,
     title: 'La abeja obrera',
     text: 'Las abejas obreras realizan distintas tareas dentro de la colmena.',
+    image: '/assets/Abeja Carpintera del Sur.png',
   },
   {
     id: 2,
     title: 'La polinización',
     text: 'Las abejas ayudan a polinizar una gran cantidad de plantas y cultivos.',
+    image: '/images/polinizacion.jpg',
   },
   {
     id: 3,
     title: 'La colmena',
     text: 'Una colonia puede trabajar de forma coordinada como una enorme comunidad.',
+    image: '/images/colmena.jpg',
   },
   {
     id: 4,
     title: 'La reina',
     text: 'La abeja reina es fundamental para mantener la colonia.',
+    image: '/images/abeja-reina.jpg',
   },
   {
     id: 5,
     title: 'La miel',
     text: 'La miel es el resultado del trabajo colectivo de miles de abejas.',
+    image: '/images/miel.jpg',
   },
 ]
 
@@ -46,7 +51,11 @@ function Carrusel() {
               onMouseEnter={() => setActiveBee(index)}
             >
               <div className="bee-hex-inner">
-                <span className="bee-icon">🐝</span>
+               <img
+                src={bee.image}
+                alt={bee.title}
+                className="bee-image"
+              />
 
                 <h3>{bee.title}</h3>
 
